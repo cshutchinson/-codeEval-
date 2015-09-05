e2f9b2d931bbd2cc7b3684e95457596b5e4738e3
Christopher Hutchinson <cshutchinson@gmail.com>
2015-09-04 22:13:25 -0600

    working cycle detection

--- detectingCycles.js
+++ detectingCycles.js
@@ -3,18 +3,18 @@ var fs  = require('fs');
 
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     var tortoise=1,
-	hare=2;
-	var mu=0;
+		hare=2,
+	 	mu=0;
 
     if (line != '') {
     	var lineArr = line.split(' ').map(function(elem){
-    		return parseInt(elem,10)
+    		return +elem;
     	});
-    	console.log(lineArr);
+    	// console.log(lineArr);
     	var length = lineArr.length-1;
     
 	    while (lineArr[tortoise%length] !== lineArr[hare%length]){
-	    	console.log('tortoise: ' + tortoise + ' hare: ' + hare);
+	    	// console.log('tortoise: ' + tortoise + ' hare: ' + hare);
 	    	tortoise++;
 	    	hare +=2 ;
 	    }
@@ -26,19 +26,24 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
     		mu ++;
 
     	}
+    	// console.log('tortoise: ' + tortoise + ' hare: ' + hare);
 
     	var lam=1;
-    	hare=tortoise;
-    	while (tortoise !== hare) {
+    	tortoise=mu;
+    	hare=tortoise+1;
+    	// console.log(lineArr[tortoise]);
+    	// console.log(lineArr[hare]);
+    	while (lineArr[tortoise] !== lineArr[hare]) {
     		hare++;
     		lam ++;
     	}
 
-    	console.log('tortoise: ' + tortoise + ' hare: ' + hare);
-    	console.log(lineArr[tortoise%length]);
-    	console.log('mu position: ' + mu);
+    	// console.log('tortoise: ' + tortoise + ' hare: ' + hare);
+    	// console.log(lineArr[tortoise%length]);
+    	// console.log('mu position: ' + mu);
 
-    	console.log('lam: ' + lam + ' mu: ' + mu);
+    	// console.log('lam: ' + lam + ' mu: ' + mu);
+    	console.log(lineArr.splice(mu,lam).join(' '));	
 
 
 		tortoise=1, hare=2, mu=0;
