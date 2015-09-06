8e7436ff59c5a97edfc3b8eef3ae59738db3b4b5
Christopher Hutchinson <cshutchinson@gmail.com>
2015-09-06 16:32:08 -0600

    one more rewrite to follow floyd algorightm more closely

--- detectingCycles.js
+++ detectingCycles.js
@@ -6,8 +6,19 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
     	var lineArr = line.split(' ').map(function(elem){
             return +elem;
         });
-        //console.log(lineArr);
-        // for (each number) move (2nd cursor) until (both are the same) 
+        console.log(lineArr);
+
+        var tortoise=0, hare=1;
+
+        
+
+
+	}
+    
+});
+
+
+// for (each number) move (2nd cursor) until (both are the same) 
         // then (move both at same speed to find the whole cycle)
         // becuse you could have 6 3 1 6 3 1 6 3 2 6 3 1
         // and on this example the answer would still be 6 3 1
@@ -22,23 +33,24 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
         //     incrent length
         //     increment hare,tortoise at same speed
         // with length known return splice(firstTortoise, length)
-        var tortoise=0, hare=1, firstTortoise=0;
-        while (lineArr[tortoise] !== lineArr[hare]){
-            hare = lineArr.indexOf(lineArr[tortoise], tortoise+1);
-            hare === -1 ? tortoise++ : null;
-        }
-        // var length=1, firstTortoise=tortoise;
-        // console.log(firstTortoise);
-        // console.log (tortoise, hare);
 
-        // while(lineArr[tortoise]===lineArr[hare]){
-        //     tortoise++, hare++;
-        //     length++;
+        // toroise is set to first element in array, hare second
+
+        // while (lineArr[tortoise] !== lineArr[hare]){
+        //     hare = lineArr.indexOf(lineArr[tortoise], tortoise+1);
+        //     hare === -1 ? tortoise++ : null;
         // }
+        // var length=1, firstTortoise=tortoise;
+        // console.log('tortoise: ' + tortoise + ' hare: '+ hare);
+        // console.log('first tortoise index: ' +firstTortoise);
 
-        console.log(lineArr.slice(tortoise, hare).join(' '));
+        // tortoise++, hare++;
+        // console.log('tortoise: ' + tortoise + ' hare: '+ hare);
+        //  while(lineArr[firstTortoise]!==lineArr[hare]){
+        //      tortoise++, hare++;
+        //      length++;
+        //  }
+        //  console.log('tortoise: ' + tortoise + ' hare: '+ hare + ' length: ' + length);
+        //  // console.log(length);
 
-
-	}
-    
-});
+        // console.log(lineArr.slice(firstTortoise, length));
